44e3d91200e2eee4c1c75d7cad6da742a9e7ce26
chaitalee ghare <chaitaleeghare16@gmail.com>
2024-03-23 14:24:30 +0530

    Add Tooltip

--- app/page.tsx
+++ app/page.tsx
@@ -9,6 +9,8 @@ import {
   Text,
   Avatar,
   rem,
+  Anchor,
+  Tooltip,
 } from "@mantine/core";
 import classes from "../styles/page.module.css";
 import "@mantine/core/styles/global.css";
@@ -101,11 +103,16 @@ export default function HomePage() {
               <Grid.Col span={{ xs: 12, sm: 6, md: 4, lg: 3 }} key={user.id}>
                 <Paper shadow="sm" withBorder p="lg" radius={rem(10)}>
                   <div className={classes.user_avatar}>
+                    <Tooltip label={user.name}>
+                    <Anchor href={`https:\\${user.website}`} target="_blank">
                     <Avatar
                       src={`https://api.dicebear.com/7.x/initials/svg?seed=${user.name}`}
                       alt={`Avatar for ${user.name}`}
                       size={rem(120)}
                     />
+                    </Anchor>
+                    </Tooltip>
+                    
                   </div>
 
                   <div className={classes.user_name}>
@@ -123,19 +130,22 @@ export default function HomePage() {
                     <div className={classes.user_info}>
                       <IconAt className={classes.tabler_icon} />
 
-                      <Text c="dimmed">{user.email}</Text>
+                      
+                      <Anchor href={`mailto:${user.email}`} underline="hover"  target="_blank" className={classes.custom_link}>{user.email}</Anchor>
+
                     </div>
 
                     <div className={classes.user_info}>
                       <IconPhoneCall className={classes.tabler_icon} />
 
-                      <Text c="dimmed">{user.phone}</Text>
+                      <Anchor href={`tel:${user.phone}`} underline="hover"  target="_blank" className={classes.custom_link}>{user.phone}</Anchor>
                     </div>
 
                     <div className={classes.user_info}>
                       <IconWorld className={classes.tabler_icon} />
 
-                      <Text c="dimmed">{user.website}</Text>
+                    
+                      <Anchor href={`https:\\${user.website}`} underline="hover" target="_blank" className={classes.custom_link}>{user.website}</Anchor>
                     </div>
                   </div>
 
